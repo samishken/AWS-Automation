a83c7a3236bd480d732a277edeb26392f163a6fe
Samuel Haile <samishken@gmail.com>
2019-05-14 22:37:15 -0400

    add more parameters to volume.sh

--- volume.sh
+++ volume.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # check if less than 2 arguments passed
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
 then
-  echo "Usage: $0 [availabilityZone] [volumeType] [volumeSize]"
+  echo "Usage: $0 [availabilityZone] [volumeType] [volumeSize] [volumeKey] [volumeName]"
   exit 1;
 fi
 
 availabilityZone=$1
 volumeType=$2
 volumeSize=$3
-# volumeName=$4   need to look into adding the fourth argument         
+volumeKey=$4
+volumeName=$5          
 
 #add ebs volume
-createVolume=`aws ec2 create-volume --availability-zone ${availabilityZone} --volume-type ${volumeType} --size ${volumeSize} --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=LFC}]'`
+createVolume=`aws ec2 create-volume --availability-zone ${availabilityZone} --volume-type ${volumeType} --size ${volumeSize} --tag-specifications 'ResourceType=volume,Tags=[{Key='${volumeKey}',Value='${volumeName}'}]'`
